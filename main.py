183c159f74c85738118e15cb35dc218d6b15bc85
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-11-24 03:31:27 +0900

    학습된 자연어 처리 및 감정 분류 모델 추가 (Git LFS 사용)

--- main.py
+++ main.py
@@ -4,26 +4,51 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from fastapi import status
 
-from text_processing.input_text_processing import *
+from text_processing.input_text_processing import text_processing
+from emotion_prediction.emotion_predict import predict_emotion
 
 app = FastAPI()
 
 class InputText(BaseModel):
     text: str
 
-@app.post("/text-processing")
-async def text_processing(inputText: InputText):
+# @app.post("/text-processing")
+# async def text_processing(inputText: InputText):
+#     try:
+#         final_correct_text = text_processing(inputText.text)
+#
+#         return JSONResponse(
+#             status_code=status.HTTP_201_CREATED,
+#             content={
+#                 "statusCode": 201,
+#                 "message": "데이터 전처리 되었습니다.",
+#                 "correct_text": final_correct_text
+#             }
+#         )
+#     except Exception as e:
+#         print(e)
+#         return JSONResponse(
+#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+#             content={
+#                 "statusCode": 500,
+#                 "data": {
+#                     "message": ['텍스트 전처리를 실패했습니다.'],
+#                 }
+#             }
+#         )
+
+@app.post("/emotion")
+def emotion_prediction(inputText: InputText):
     try:
-        correct_spell = await orthography_examination(inputText.text)
-        correct_spacing = word_spacing_correction(correct_spell)
-        final_correct_text = morpheme_analyzer(correct_spacing)
+        correct_text = text_processing(inputText.text)
+        emotion_result = predict_emotion([correct_text])
 
         return JSONResponse(
             status_code=status.HTTP_201_CREATED,
             content={
                 "statusCode": 201,
-                "message": "데이터 전처리 되었습니다.",
-                "correct_text": final_correct_text
+                "message": "사용자의 감정이 정상적으로 예측되었습니다.",
+                "emotion": emotion_result
             }
         )
     except Exception as e:
@@ -33,7 +58,7 @@ async def text_processing(inputText: InputText):
             content={
                 "statusCode": 500,
                 "data": {
-                    "message": ['텍스트 전처리를 실패했습니다.'],
+                    "message": ['서버 상의 이유로 감정 예측을 실패하였습니다.'],
                 }
             }
         )
