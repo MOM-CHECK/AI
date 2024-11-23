4bd3a87977614af073eca6d2ed67ab95de30a090
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-11-23 21:19:11 +0900

    feat: 맞춤법 교정 로직 생성

--- text_processing/input_text_processing.py
+++ text_processing/input_text_processing.py
@@ -1,7 +1,16 @@
+import re
+import json
+import logging
 from pykospacing import Spacing
+from kospellpy import spell_init
 
 def word_spacing_correction(text):
     without_spacing_text = text.replace(" ", "")
     spacing = Spacing()
     correct_spacing_text = spacing(without_spacing_text)
     return correct_spacing_text
+
+async def orthography_examination(text):
+    spell_checker = spell_init()
+    correct_spell_text = spell_checker(text)
+    return correct_spell_text
