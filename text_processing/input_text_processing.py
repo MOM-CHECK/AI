06a96955c21f19604123deda63fa52f762fbebdc
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-11-24 01:48:41 +0900

    feat: 형태소 분석기 로직 생성

--- text_processing/input_text_processing.py
+++ text_processing/input_text_processing.py
@@ -3,6 +3,7 @@ import json
 import logging
 from pykospacing import Spacing
 from kospellpy import spell_init
+from konlpy.tag import Okt
 
 def word_spacing_correction(text):
     without_spacing_text = text.replace(" ", "")
@@ -14,3 +15,10 @@ async def orthography_examination(text):
     spell_checker = spell_init()
     correct_spell_text = spell_checker(text)
     return correct_spell_text
+
+def morpheme_analyzer(text):
+    okt = Okt()
+    morphemes = okt.morphs(text, stem=True)
+    morpheme_text = ' '.join(morphemes)
+    morpheme_text = re.sub(r'\s+\.', '.', morpheme_text)
+    return morpheme_text
