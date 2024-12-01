6431e712d687ad68459fdb7930c7f40b734d28cc
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2024-12-01 20:55:21 +0900

    feat: 감정 예측 API 생성

--- main.py
+++ main.py
@@ -4,51 +4,38 @@ from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from fastapi import status
 
-from text_processing.input_text_processing import text_processing
-from emotion_prediction.emotion_predict import predict_emotion
+from emotion_prediction.emotion_predict import predict
+
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080/emotions"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class InputText(BaseModel):
     text: str
 
-# @app.post("/text-processing")
-# async def text_processing(inputText: InputText):
-#     try:
-#         final_correct_text = text_processing(inputText.text)
-#
-#         return JSONResponse(
-#             status_code=status.HTTP_201_CREATED,
-#             content={
-#                 "statusCode": 201,
-#                 "message": "데이터 전처리 되었습니다.",
-#                 "correct_text": final_correct_text
-#             }
-#         )
-#     except Exception as e:
-#         print(e)
-#         return JSONResponse(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             content={
-#                 "statusCode": 500,
-#                 "data": {
-#                     "message": ['텍스트 전처리를 실패했습니다.'],
-#                 }
-#             }
-#         )
-
-@app.post("/emotion")
+@app.post("/emotions")
 def emotion_prediction(inputText: InputText):
     try:
-        correct_text = text_processing(inputText.text)
-        emotion_result = predict_emotion([correct_text])
+        emotion = predict(inputText)
 
         return JSONResponse(
             status_code=status.HTTP_201_CREATED,
             content={
                 "statusCode": 201,
                 "message": "사용자의 감정이 정상적으로 예측되었습니다.",
-                "emotion": emotion_result
+                "emotion": emotion
             }
         )
     except Exception as e:
